ef1695c1bc052c8d3ef81b713a7b397a74dba1f5
Andres Romero <andres.romero@endava.com>
2018-02-21 12:13:03 -0500

    Adding module dependency

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
+var test= require('test');
 var assert = require('assert')
 
 function test() {
-    assert.equal(2 + 2, 5);
+    assert.equal(2 + 2, 4);
 }
 
 if (module == require.main) require('test').run(test);
