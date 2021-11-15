2b8874e5009d34927cac480f2e582e2c6ed3ef96
uledwin <93341223+uledwin@users.noreply.github.com>
2021-11-15 16:58:44 -0500

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('<h1>Hello world 3.0<h1>\n');
+    res.send('<h1>Hello world 2.0<h1>\n');
 });
 
 app.listen(PORT, HOST);
